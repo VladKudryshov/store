51b535d525b2409940c8c577423e696fa700b741
kudr9tov <uladzislau.kudrashou@instinctools.ru>
2019-04-23 16:22:20 +0300

    fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
-sudo docker run --name order-db -e POSTGRES_PASSWORD=1 -p 5432:5432 -d postgres:9.6 || docker start order-db
+#sudo docker run --name order-db -e POSTGRES_PASSWORD=1 -p 5432:5432 -d postgres:9.6 || docker start order-db
 
 sudo docker rm -f order-back
 sudo docker build -t order-back .
