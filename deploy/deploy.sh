bfea746ad55ab92f51f3251ea2831e2d8bd2c69d
kudr9tov <uladzislau.kudrashou@instinctools.ru>
2019-06-04 14:21:20 +0300

    fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,6 +8,8 @@ sudo docker run --name order-db -e POSTGRES_PASSWORD=1 -p 5432:5432 -d postgres:
 #FTP
 #docker run --name server-ftp -d -p 21:21 -p 21100-21110:21100-21110 -v /opt/server/nginx/html/files:/home/vsftpd/reports -e FTP_USER=reports -e FTP_PASS=1 -e LOG_STDOUT=true -e PASV_ADDRESS=172.17.255.255 -e PASV_MIN_PORT=21100 -e PASV_MAX_PORT=21110 -e FILE_OPEN_MODE=0755 -e LOCAL_UMASK=002 --restart=always fauria/vsftpd || docker restart server-ftp
 
+cd
+cd ..
 cd ../home/kudr9tov/projects/store
 git pull
 sudo docker stop order-back
